f53e6592e2cd7fea0b5987255c2f73d69969606e
fesson <cccp0110@gmail.com>
2021-12-30 10:57:48 +0300

    redux + column resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,27 +1,29 @@
 const CODES = { A: 65, Z: 90 }
+const DEFAULT_WIDTH = 120
 
-// function toCell(_, index) {
-//   //createCell
-//   return `
-//     <div class="cell" contenteditable="" data-col="${index}">
-
-//     </div>
-//     `
-// }
-function toCell(row) {
+function toCell(state, row) {
   //createCell
-  return function (_, index) {
+  return function (_, col) {
+    const width = getWidth(state.colState, col)
     return `
-    <div class="cell" contenteditable="" data-col="${index}" data-id="${row}:${index}" data-type="cell">     
+    <div class="cell"
+     contenteditable=""
+      data-col="${col}"
+       data-id="${row}:${col}"
+        data-type="cell"
+        style="width: ${width}">     
     </div>
     `
   }
 }
 
-function toColumn(letter, index) {
-  // createCol
+function toColumn({ letter, index, width }) {
   return `
-     <div class="column" data-type="resizable" data-col="${index}">
+     <div
+      class="column"
+      data-type="resizable"
+      data-col="${index}"
+      style = "width: ${width}">
         ${letter}       
         <div class="col-resize" data-resize="col"></div>
      </div>
@@ -47,21 +49,35 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index) //get letter
 }
 
-export function createTable(rowsCount = 15) {
+function getWidth(state, index) {
+  return (state[index] || DEFAULT_WIDTH) + 'px'
+}
+function withWidthFrom(state) {
+  return function (letter, index) {
+    return {
+      letter,
+      index,
+      width: getWidth(state.colState, index),
+    }
+  }
+}
+
+export function createTable(rowsCount = 15, state = {}) {
   const colsCount = CODES.Z - CODES.A + 1 // для буквы Z
   const rows = []
   //вычисляем буквы из их кода
   const colsLetter = new Array(colsCount)
     .fill('')
     .map(toChar) // замена.map((_, index) => String.fromCha...
-    .map(toColumn) // замена .map((letter) => createCol(letter))
+    .map(withWidthFrom(state))
+    .map(toColumn)
     .join('') // в итоге это ячейки строки для row-data
 
   //первая строка, техническая с буквами в row-data
   rows.push(createRow('', colsLetter))
 
   for (let row = 0; row < rowsCount; row++) {
-    const cells = new Array(colsCount).fill('').map(toCell(row)).join('')
+    const cells = new Array(colsCount).fill('').map(toCell(state, row)).join('')
 
     rows.push(createRow(row + 1, cells))
   }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import { Excel } from './components/excel/Excel'
 import { Formula } from './components/formula/Formula'
 import { Header } from './components/header/Header'
+import { rootReducer } from './components/redux/rootReducer'
 import { Table } from './components/table/Table'
 import { Toolbar } from './components/toolbar/Toolbar'
+import { createStore } from './core/createStore'
+import { storage } from './core/utils'
 import './scss/index.scss'
+
+const store = createStore(rootReducer, storage('excel-state'))
+
+store.subscribe((state) => {
+  console.log('State from Excel', state)
+  storage('excel-state', state)
+})
 
 const excel = new Excel('#app', {
   components: [Header, Toolbar, Formula, Table],
+  store,
 })
 excel.render()
